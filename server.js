6f0c70dbdaac72ca9c248332a6560201f4194bb5
jChauhan18 <jlinux1999@gmail.com>
2024-07-25 21:56:31 +0530

    Made some changes for  vercel Deployment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const multer = require('multer');
-const cloudinary = require('cloudinary').v2;
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 require("dotenv").config();
 // const PORT = 3001
@@ -12,23 +12,20 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // mongo Conn (add your own url -_-)
-mongoose.connect(
-  process.env.MONGODB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // img schema
 const imageSchema = new mongoose.Schema({
   url: String,
 });
-const Image = mongoose.model('Image', imageSchema);
+const Image = mongoose.model("Image", imageSchema);
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
@@ -38,29 +35,27 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-})
-app.get('/', (req, res) => {
-  res.render('index', { title: 'CloudPic' });
+});
+app.get("/", (req, res) => {
+  res.render("index", { title: "CloudPic" });
 });
 
-app.post('/upload', upload.single('file'), async (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
   try {
     if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
+      return res.status(400).json({ error: "No file uploaded" });
     }
 
     const result = await new Promise((resolve, reject) => {
-      cloudinary.uploader.upload_stream(
-        { resource_type: 'auto' },
-        (error, result) => {
+      cloudinary.uploader
+        .upload_stream({ resource_type: "auto" }, (error, result) => {
           if (error) {
             return reject(error);
           }
           resolve(result);
-        }
-      ).end(req.file.buffer);
+        })
+        .end(req.file.buffer);
     });
-    
 
     // Saving img URL in MongoDB
     const image = new Image({ url: result.secure_url });
@@ -68,11 +63,13 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
     res.json({ url: result.secure_url });
   } catch (error) {
-    console.error('Error uploading file:', error);
-    res.status(500).json({ error: 'Error uploading file' });
+    console.error("Error uploading file:", error);
+    res.status(500).json({ error: "Error uploading file" });
   }
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
+
+module.exports = app;
